47e74500edfeae82d64916fa8163ebf679a8399e
Sandeep9989 <94556357+Sandeep9989@users.noreply.github.com>
2021-11-19 12:48:50 -0600

    removed mysql

--- LoanApprovalSystem/LoanApprovalSystem/wsgi.py
+++ LoanApprovalSystem/LoanApprovalSystem/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoanApprovalSystem.settings_mysql')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LoanApprovalSystem.settings')
 
 application = get_wsgi_application()
