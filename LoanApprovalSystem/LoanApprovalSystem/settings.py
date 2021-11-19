2f6ff0f6de01f53cc0725427aa2acf76f7dd71d0
Sandeep9989 <94556357+Sandeep9989@users.noreply.github.com>
2021-11-19 12:08:53 -0600

    Update settings.py
    commit

--- LoanApprovalSystem/LoanApprovalSystem/settings.py
+++ LoanApprovalSystem/LoanApprovalSystem/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -130,4 +130,3 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "loanchecking123@gmail.com"
 EMAIL_HOST_PASSWORD ="loanchecking@123@123"
-
